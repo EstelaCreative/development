9d4df5130f0ba8f06abeed387fda7bdbc63a0462
Estela Creative <34207144+EstelaCreative@users.noreply.github.com>
2018-01-17 23:37:58 +0000

    Got cake experience - 17/01/2018
    Playing with HTML, CSS, JavaScript and Illustration.

--- example3.js
+++ example3.js
@@ -1,17 +1,4 @@
-//var now = 2017;
-//var birthYear = now - 44;
-//birthYear = now - 44 * 2;
-//console.log(birthYear);
 
-//var now = 2017;
-//var birthYear = now - 44;
-//console.log(birthYear);
-
-
-var now = 2017;
-var birthYear = 1973;
-var years = now - birthYear;
-console.log(years);
 
 var years = prompt('Do you like cake?');
-console.log(years);
+console.log(answer);
